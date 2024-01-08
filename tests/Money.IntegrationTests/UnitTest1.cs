3c6d419e31158cddf9cbc2e326667cae628d34be
Lucas Rufo <rifelu25@outlook.com>
2024-01-08 14:34:49 -0300

    feat: create projects

---
+++ tests/Money.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Money.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/Money.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Money.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
